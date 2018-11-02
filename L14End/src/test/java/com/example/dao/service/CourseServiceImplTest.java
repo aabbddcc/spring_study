8808c3d500b4595012e041c4431778dbda546eb2
BIGSING <pushebp@163.com>
2018-11-02 18:33:00 +0800

    1

---
+++ L14End/src/test/java/com/example/dao/service/CourseServiceImplTest.java
@@ -0,0 +1,34 @@
+package com.example.dao.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CourseServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void deleteByIds() {
+    }
+
+    @Test
+    public void deleteByIds1() {
+    }
+
+    @Test
+    public void queryList() {
+    }
+
+    @Test
+    public void queryList1() {
+    }
+}
---
+++ L20Tomcat/src/main/java/com/example/helloworld/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+运行后在浏览器中访问：
+http://localhost:8080
+http://localhost:8080/index
+*/
+@RestController
+public class HelloController {
+    @RequestMapping({"/", "/index"})
+    private String index() {
+        return "hello world2";
+    }
+}
